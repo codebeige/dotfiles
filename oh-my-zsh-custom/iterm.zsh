c4be912b292cc3517e2dc94e2695527eeb61dec4
Tibor Claassen <tibor@gmah.net>
2014-06-22 14:46:11 +0200

    Change cursor shape in iTerm, too

--- oh-my-zsh-custom/iterm.zsh
+++ oh-my-zsh-custom/iterm.zsh
@@ -1,14 +1,45 @@
-# switch to iTerm profile by name
-chprofile() { echo -e "\033]50;SetProfile=$1\a" }
+if [[ "$TERM_PROGRAM" == "iTerm.app" ]]; then
+  # switch to iTerm profile by name
+  change_profile() { print -n -- "\033]50;SetProfile=$1\a" }
 
-# toggle solarized theme
-toggle_solarized_mode() {
-  case ${SOLARIZED_THEME:-dark} in
-    light) export SOLARIZED_THEME=dark ;;
-    *)     export SOLARIZED_THEME=light;;
-  esac
-  chprofile "Solarized ${(C)SOLARIZED_THEME}"
-  source "$ZSH/themes/$ZSH_THEME.zsh-theme"
-  chprofile "Solarized ${(C)SOLARIZED_THEME}"
-}
-alias tgbg=toggle_solarized_mode
+  # set cursor shape (0: block, 1:vertical bar, 2:underline)
+  change_cursor_shape() { print -n -- "\033]50;CursorShape=$1\a" }
+
+  # clear scrollback history
+  clear_scrollback() { print -n -- "\033]50;ClearScrollback\a" }
+
+  # toggle solarized theme
+  toggle_solarized_mode() {
+    case ${SOLARIZED_THEME:-dark} in
+      light) export SOLARIZED_THEME=dark ;;
+      *)     export SOLARIZED_THEME=light;;
+    esac
+    change_profile "Solarized ${(C)SOLARIZED_THEME}"
+    clear_scrollback
+    source "$ZSH/themes/$ZSH_THEME.zsh-theme"
+  }
+  alias tgm=toggle_solarized_mode
+
+  # change cursor according to vi-mode
+  function zle-line-init {
+    change_cursor_shape 2
+  }
+
+  function zle-line-finish {
+    change_cursor_shape 0
+  }
+
+  function zle-keymap-select {
+    case $KEYMAP in
+      vicmd)      change_cursor_shape 0;;
+      viins|main) change_cursor_shape 2;;
+    esac
+
+    zle reset-prompt
+    zle -R
+  }
+
+  zle -N zle-line-init
+  zle -N zle-line-finish
+  zle -N zle-keymap-select
+fi
