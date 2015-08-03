0dd7f7e8b0e2a409c7a2ed302b2e1535faa4472e
Tibor Claassen <tibor@gmah.net>
2015-08-03 12:55:31 +0300

    Use latest nrepl with lein commands

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,2 +1,3 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [refactor-nrepl    "1.2.0-SNAPSHOT"]]}}
+                  [refactor-nrepl    "1.2.0-SNAPSHOT"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
