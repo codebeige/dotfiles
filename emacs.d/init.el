c996fb5010028c899fffffa6aa42aaaf790ecff0
Tibor Claassen <tibor@gmah.net>
2015-09-15 08:21:50 +0200

    [emacs] Update Melpa URL

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -7,7 +7,7 @@
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
                          ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
 (defvar my-packages '(cider
