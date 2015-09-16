efd79f2675b45098e261eda035603839c8e5e927
Tibor Claassen <tibor@gmah.net>
2015-09-16 18:33:04 +0200

    [emacs] Move all tempfiles to an invisible directory

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -50,3 +50,13 @@
   (if (key-binding [escape]) [escape] (kbd "C-g")))
 (define-key key-translation-map (kbd "C-ä") 'escape-or-quit)
 (global-set-key (kbd "C-ü") (kbd "C-]"))
+
+;; tempfiles
+(defconst emacs-tmp-dir "~/.emacs-tmp/")
+(setq backup-directory-alist `((".*" . ,emacs-tmp-dir)))
+(setq auto-save-file-name-transforms `((".*" ,emacs-tmp-dir t)))
+(setq auto-save-list-file-prefix emacs-tmp-dir)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
