08612fa5191da6a5d43a5050133007370d62a071
Tibor Claassen <tibor@gmah.net>
2015-09-16 16:19:31 +0200

    [emacs] Use Solarized theme

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -22,8 +22,14 @@
 (when (not package-archive-contents) (package-refresh-contents))
 (dolist (p my-packages) (when (not (package-installed-p p)) (package-install p)))
 
-;; appearance
+;; GUI
 (setq ring-bell-function 'ignore)
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
+
+;; appearance
+(load-theme 'solarized-light t)
+(set-face-attribute 'default nil :family "Source Code Pro" :height 145)
 
 ;; global navigation
 (setq ido-enable-flex-matching t)
@@ -43,22 +49,3 @@
   (if (key-binding [escape]) [escape] (kbd "C-g")))
 (define-key key-translation-map (kbd "C-ä") 'escape-or-quit)
 (global-set-key (kbd "C-ü") (kbd "C-]"))
-
-;; customized
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (wombat)))
- '(custom-safe-themes
-   (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(inhibit-startup-screen t)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#242424" :foreground "#f6f3e8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "Source Code Pro")))))
