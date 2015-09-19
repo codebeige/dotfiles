1df31a311de88d30ada6929823aff873c9874eeb
Tibor Claassen <tibor@gmah.net>
2015-09-19 10:13:26 +0200

    [emacs] Hide files created by auto-save

--- emacs.d/config/tmp.el
+++ emacs.d/config/tmp.el
@@ -1,1 +1,11 @@
-(setq backup-directory-alist `((".*" . ,(concat user-emacs-directory "backups/"))))
+(defun user-emacs-tmp-dir (name)
+  (let ((dir (concat user-emacs-directory name "/")))
+    (unless (file-exists-p dir)
+      (make-directory dir))
+    dir))
+
+(setq backup-directory-alist
+      `((".*" . ,(user-emacs-tmp-dir "backups"))))
+
+(setq auto-save-file-name-transforms
+      `((".*" ,(user-emacs-tmp-dir "auto-save-files")) t))
