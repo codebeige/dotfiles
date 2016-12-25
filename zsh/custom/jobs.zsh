1afd54dd8bc9da0a10445fa05d7d6c39059618b8
Tibor Claassen <tibor@gmah.net>
2016-12-25 19:18:41 +0100

    [zsh] Aliases for job control

---
+++ zsh/custom/jobs.zsh
@@ -0,0 +1,8 @@
+alias j='jobs -l'
+alias kill!='kill -KILL'
+
+alias 1='fg %1' 11='bg %1'
+alias 2='fg %2' 22='bg %2'
+alias 3='fg %3' 33='bg %3'
+alias 4='fg %4' 44='bg %4'
+alias 5='fg %5' 55='bg %5'
