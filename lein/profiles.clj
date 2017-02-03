27fc0db24b878acce805efcae8ce116ff555c1ea
Tibor Claassen <tibor@gmah.net>
2017-02-03 21:11:07 +0100

    [lein] Upgrade cider plugin

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,7 +3,7 @@
          [org.clojure/tools.nrepl "0.2.10"]]
 
         :plugins
-        [[cider/cider-nrepl "0.12.0"]]
+        [[cider/cider-nrepl "0.14.0"]]
 
         :repl-options
         {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
