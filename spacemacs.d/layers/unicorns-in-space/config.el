bc6dcf23e2a794ed4570000d7efaf004d77e18ba
Tibor Claassen <tc@codebeige.net>
2017-07-15 12:08:40 +0200

    [spacemacs] Key bindings for macOS

--- spacemacs.d/layers/unicorns-in-space/config.el
+++
@@ -1,9 +0,0 @@
-(add-hook 'cider-repl-mode-hook #'smartparens-strict-mode)
-(add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
-(add-hook 'cider-mode-hook #'eldoc-mode)
-
-(setq cider-repl-pop-to-buffer-on-connect t)
-
-(add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
-
-
