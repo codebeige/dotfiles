c551a55933dda2890ca31c7f4b74eb652765ab35
Tibor Claassen <tc@codebeige.net>
2017-07-22 19:52:20 +0200

    [spacemacs] Use default font weight

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -150,7 +150,7 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Space Code Pro"
+   dotspacemacs-default-font '("Source Code Pro"
                                :size 15
                                :weight normal
                                :width normal
