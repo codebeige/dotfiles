00f1ed80635b9003cb50ecf0202b583ab2cb2c04
Tibor Claassen <tc@codebeige.net>
2017-08-30 21:12:37 +0200

    [spacemacs] Harmonize indentation for web files

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -336,8 +336,17 @@ you should place your code here."
   (setq neo-theme 'nerd
         magit-repository-directories '("~/src/" "~/lab/"))
 
-  (setq-default js2-basic-offset 2
-                js-indent-level 2)
+  (setq-default standard-indent 2
+                tab-width 2
+                indent-tabs-mode nil
+                js-indent-level 2
+                js2-basic-offset 2
+                js2-strict-semi-warning nil
+                js2-missing-semi-one-line-override nil
+                web-mode-markup-indent-offset 2
+                web-mode-css-indent-offset 2
+                web-mode-code-indent-offset 2
+                web-mode-indent-style 2)
 
   (spacemacs/toggle-evil-cleverparens-on)
   (add-hook 'prog-mode-hook #'evil-cleverparens-mode)
