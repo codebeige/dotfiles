2d9c15d3e3202e94c76773191f6633f7824efb10
Tibor Claassen <tibor@pitch.io>
2018-09-30 14:43:13 +0200

    [zsh] Set up editor configuration directories

--- zsh/config/editor.zsh
+++ zsh/config/editor.zsh
@@ -3,3 +3,9 @@ export VISUAL='nvim'
 
 alias vim=nvim
 alias vi=nvim
+
+export VIMCONFIG=~/.vim
+[[ ! -d $VIMCONFIG ]] && mkdir -p $VIMCONFIG
+
+export VIMDATA=~/.local/share/vim
+[[ ! -d $VIMDATA ]] && mkdir -p $VIMDATA
