4db564f1464633acca96f615de60dbd9f947670c
Tibor Claassen <tibor@pitch.io>
2019-02-17 21:55:31 +0100

    [zsh] Add fzf integration

---
+++ zsh/config/fzf.zsh
@@ -0,0 +1,15 @@
+# Config
+# ------
+export FZF_DEFAULT_OPTS='--height=40% --layout=reverse --border --multi'
+
+if (( $+commands[rg] )); then
+  export FZF_DEFAULT_COMMAND='rg --files --hidden --glob "!.git/*"'
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
