82c1c43e331ec857f5ce0fd45f83012db115609d
Tibor Claassen <tc@codebeige.net>
2019-11-14 09:54:17 +0100

    [zsh] Disable suffix aliases

--- zsh/config/suffix.zsh
+++
@@ -1,5 +0,0 @@
-alias -s vim=$EDITOR zsh=$EDITOR sh=$EDITOR
-alias -s txt=$EDITOR md=$EDITOR markdown=$EDITOR
-alias -s html=$EDITOR js=$EDITOR css=$EDITOR scss=$EDITOR
-alias -s clj=$EDITOR cljs=$EDITOR cljc=$EDITOR boot=$EDITOR
-alias -s rb=$EDITOR erb=$EDITOR haml=$EDITOR rake=$EDITOR
