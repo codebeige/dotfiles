0d33b7d6e0eecc6b0f3f9286db216a44e7df82c6
Tibor Claassen <tc@codebeige.net>
2021-05-06 00:31:58 +0200

    [nvim] Repeat cycle collection mapping

--- nvim/pack/config/start/lspconfig/lua/lspconfig/clojure.lua
+++ nvim/pack/config/start/lspconfig/lua/lspconfig/clojure.lua
@@ -1,6 +1,20 @@
 local M = {}
 
-function default_params()
+function M.buf_set_keymap_repeat(bufnr, mode, lhs, rhs, opts)
+  vim.api.nvim_buf_set_keymap(
+    bufnr,
+    mode,
+    lhs,
+    string.format(
+      '%s<Cmd>silent! call repeat#set("%s")<CR>',
+      rhs,
+      string.gsub(lhs, '<', [[\<]])
+    ),
+    opts
+  )
+end
+
+local function default_params()
   local cursor = vim.api.nvim_win_get_cursor(0)
   return {
     vim.uri_from_bufnr(0),
@@ -10,10 +24,13 @@ function default_params()
 end
 
 function M.cycle_coll()
+  local winview = vim.fn.winsaveview()
+  vim.cmd('normal vaf%v')
   vim.lsp.buf.execute_command{
     command = "cycle-coll",
     arguments = default_params()
   }
+  vim.fn.winrestview(winview)
 end
 
 return M
