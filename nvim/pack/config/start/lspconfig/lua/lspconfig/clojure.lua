0cefd24cff277d17a2e0413adbab05b3cfbed8ac
Tibor Claassen <tc@codebeige.net>
2021-05-13 19:36:16 +0200

    [nvim] Simplify LSP command call

--- nvim/pack/config/start/lspconfig/lua/lspconfig/clojure.lua
+++ nvim/pack/config/start/lspconfig/lua/lspconfig/clojure.lua
@@ -5,40 +5,42 @@ function M.buf_set_keymap_repeat(bufnr, mode, lhs, rhs, opts)
     bufnr,
     mode,
     lhs,
-    string.format(
-      '%s<Cmd>silent! call repeat#set("%s")<CR>',
-      rhs,
-      lhs:gsub('<', [[\<]])
-    ),
+    string.format("%s<Cmd>silent! call repeat#set('%s')<CR>", rhs, lhs),
     opts
   )
 end
 
-local function default_params()
-  local cursor = vim.api.nvim_win_get_cursor(0)
-  return {
-    vim.uri_from_bufnr(0),
-    cursor[1] - 1,
-    cursor[2]
-  }
+local function pos_comparator(left, right)
+  if left[1] == right[1] then
+    return left[2] < right[2]
+  end
+  return left[1] < right[2]
 end
 
-local function cursor_on_opening()
-  local column = vim.api.nvim_win_get_cursor(0)[2] + 1
-  local line = vim.api.nvim_get_current_line():sub(column)
-  return line:find('^[([{]') or line:find('^#{')
+local function search_pos_open()
+  local opts = "nzbcW"
+  local positions = {
+    vim.fn.searchpairpos([[\M(]], "",[[\M)]], opts),
+    vim.fn.searchpairpos([[\M[]], "",[=[\M]]=], opts),
+    vim.fn.searchpairpos([[\M#\?{]], "",[[\M}]], opts),
+  }
+  table.sort(positions, pos_comparator)
+  return positions[#positions]
 end
 
 function M.cycle_coll()
-  local winview = vim.fn.winsaveview()
-  if not cursor_on_opening() then
-    vim.cmd('keepjumps normal (')
+  local pos = search_pos_open()
+  if pos[1] == 0 then
+    return
   end
   vim.lsp.buf.execute_command{
     command = "cycle-coll",
-    arguments = default_params()
+    arguments = {
+      vim.uri_from_bufnr(),
+      pos[1] - 1,
+      pos[2] - 1,
+    },
   }
-  vim.fn.winrestview(winview)
 end
 
 return M
