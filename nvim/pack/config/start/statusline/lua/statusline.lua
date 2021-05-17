2c696d7cd31e65e7ebe93cc8017099428dfa53c4
Tibor Claassen <tc@codebeige.net>
2021-05-17 23:38:09 +0200

    [nvim] Always show path in statusline

--- nvim/pack/config/start/statusline/lua/statusline.lua
+++ nvim/pack/config/start/statusline/lua/statusline.lua
@@ -20,11 +20,32 @@ function M.git_head()
   return string.format("[%s]", head)
 end
 
-function cwd()
-  if vim.bo.filetype == 'netrw' then
-    return vim.fn.expand("%:p")
+local function bufname(buffer)
+  local buffer = buffer or "%"
+  local bufname = vim.fn.bufname(buffer)
+  local filetype = vim.api.nvim_buf_get_option(buffer, "filetype")
+  if filetype == "netrw" and bufname == "" then
+    return vim.api.nvim_buf_get_var(buffer, "netrw_curdir")
   end
-  return vim.fn.expand("%:p:h")
+  return bufname
+end
+
+function M.filepath()
+  local filepath = vim.fn.fnamemodify(bufname(), ":.")
+  local filetype = vim.bo.filetype
+  if filetype == "help" then
+    return vim.fn.fnamemodify(filepath, ":t")
+  end
+  return filepath
+end
+
+function buffer_dir(buffer)
+  local bufname = vim.fn.fnamemodify(bufname(buffer), ":p")
+  local filetype = vim.api.nvim_buf_get_option(buffer, "filetype")
+  if filetype == "netrw" then
+    return vim.fn.fnamemodify(vim.fn.bufname, ":p")
+  end
+  return vim.fn.fnamemodify(vim.fn.bufname, ":p:h")
 end
 
 local git_cmd = [[
@@ -32,22 +53,24 @@ local git_cmd = [[
 ]]
 
 function M.git_update(buffer)
-  local succ, dir = pcall(cwd)
-  if not succ then return end
-  vim.fn.jobstart(git_cmd, {
-    cwd = dir,
-    on_stdout = function(_, data, name)
-      local head = data[1]
-      if head ~= "" then
-        vim.api.nvim_buf_set_var(buffer, "git_head", head)
-      end
-    end,
-    on_stderr = function(_, data, _)
-      if data[1] ~= "" then
-        pcall(vim.api.nvim_buf_del_var, buffer, "git_head")
-      end
-    end,
-  })
+  vim.api.nvim_buf_set_var("%", "git_head", "foo")
+  -- local succ, dir = pcall(buffer_dir, buffer)
+  -- if succ then
+  --   vim.fn.jobstart(git_cmd, {
+  --     cwd = dir,
+  --     on_stdout = function(_, data, name)
+  --       local head = data[1]
+  --       if head ~= "" then
+  --         vim.api.nvim_buf_set_var(buffer, "git_head", head)
+  --       end
+  --     end,
+  --     on_stderr = function(_, data, _)
+  --       if data[1] ~= "" then
+  --         pcall(vim.api.nvim_buf_del_var, buffer, "git_head")
+  --       end
+  --     end,
+  --   })
+  -- end
 end
 
 return M
