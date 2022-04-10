9548f6780784474cfd57c2f70b6f59fbf12cb312
Tibor Claassen <tc@codebeige.net>
2022-04-10 21:06:54 +0200

    [kitty] Switch theme with fzf

---
+++ zsh/config/plugins/base16-kitty/base16-kitty.plugin.zsh
@@ -0,0 +1,21 @@
+[[ -v KITTY_PID ]] || return
+
+export KITTY_THEMES=~/.config/kitty/themes
+
+function kitty-set-theme {
+  local name=${1:-$(cat ~/.colortheme 2>/dev/null)}
+  echo ${name:=base16-tomorrow-night-eighties-256} >| ~/.colortheme
+  kitty @ set-colors --all --configured $KITTY_THEMES/$name.conf
+}
+
+
+function kitty-switch-theme {
+  local name=$(
+    ls $KITTY_THEMES/ \
+    | xargs basename -s .conf \
+    | fzf --preview "head -n 20 $KITTY_THEMES/{}.conf && kitty @ set-colors $KITTY_THEMES/{}.conf"
+  )
+  kitty-set-theme $name
+}
+
+kitty-set-theme
