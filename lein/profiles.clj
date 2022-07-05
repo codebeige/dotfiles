f35a91649b65f8954fb614b50c5c9fa66250740b
Tibor Claassen <tc@codebeige.net>
2022-07-05 20:09:07 +0200

    [clojure] Bump dependencies & cleanup

--- lein/profiles.clj
+++
@@ -1,9 +0,0 @@
-{:user {:dependencies
-        [[cider/piggieback "0.5.3"]
-         [com.cemerick/piggieback "0.2.1"]]
-
-        :plugins
-        [[cider/cider-nrepl "0.28.3"]]
-
-        :repl-options
-        {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}}
