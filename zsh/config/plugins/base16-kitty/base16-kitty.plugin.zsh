2e90c86e01bee50e15db2e1262bf994035396000
Tibor Claassen <tc@codebeige.net>
2023-05-12 13:25:50 +0200

    [kitty] Invert cursor color

--- zsh/config/plugins/base16-kitty/base16-kitty.plugin.zsh
+++ zsh/config/plugins/base16-kitty/base16-kitty.plugin.zsh
@@ -6,6 +6,8 @@ function kitty-set-theme {
   local name=${1:-$(cat ~/.colortheme 2>/dev/null)}
   echo ${name:=base16-tomorrow-night-eighties-256} >| ~/.colortheme
   kitty @ set-colors --all --configured $KITTY_THEMES/$name.conf
+  local background_color=$(kitty @ get-colors | rg '^background\s+' --replace '')
+  kitty @ set-colors --all --configured cursor_text_color=$background_color
 }
 
 
