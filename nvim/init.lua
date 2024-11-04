c52634303b757b968c7dcbe5e22f87151ceb82a7
Tibor Claassen <tc@codebeige.net>
2024-11-04 13:48:27 +0100

    [nvim] Fall back to Github when there is no rig dev dir

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,26 +1,44 @@
-local function init ()
-  local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-  if not vim.loop.fs_stat(lazypath) then
-    print('Fetching folke/lazy.nvim from Github...')
-    vim.fn.system({
-      'git',
-      'clone',
-      '--filter=blob:none',
-      'https://github.com/folke/lazy.nvim.git',
-      '--branch=stable', -- latest stable release
-      lazypath,
-    })
-    print('Successfully installed folke/lazy.nvim at', lazypath)
+function fetch(repo, path)
+  local url = 'https://github.com/' .. repo .. '.git'
+  if not vim.loop.fs_stat(path) then
+    print('Fetching ' .. url .. '...')
+    local result = vim.system(
+      {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        '--branch=stable',
+        url,
+        path,
+      }, { text = true }
+    ):wait()
+    if result.code == 0 then
+      print('Successfully installed ' .. repo .. ' at ' .. path .. '.')
+    else
+      error('[' .. result.code .. '] ' .. result.stderr)
+    end
   end
-  vim.opt.rtp:prepend(lazypath)
 end
 
-if pcall(init) then
-  local rig_plugin_dir = "~/src/rig.nvim"
-  vim.opt.rtp:prepend(rig_plugin_dir)
-  require('rig').setup()
+local function init ()
+  local rig_plugin_dir
+  local rig_plugin_dev_dir = vim.fs.normalize('~/src/rig.nvim')
 
+  if vim.loop.fs_stat(rig_plugin_dev_dir) then
+    rig_plugin_dir = rig_plugin_dev_dir
+  else
+    rig_plugin_dir = vim.fn.stdpath('data') .. '/lazy/rig.nvim'
+    fetch('codebeige/rig.nvim', rig_plugin_dir)
+    dofile(rig_plugin_dir .. "/build.lua")
+  end
+  vim.opt.rtp:prepend(rig_plugin_dir)
+
+  require('rig').setup()
   require('config.base').setup()
+
+  local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+  fetch('folke/lazy.nvim', lazypath)
+  vim.opt.rtp:prepend(lazypath)
 
   require('lazy').setup('plugins', {
     dev = {
@@ -33,8 +51,12 @@ if pcall(init) then
       backdrop = 100,
     },
   })
+end
 
+local success, message = pcall(init)
+
+if success then
   require('config').setup()
 else
-  print('Error: unable to install. Are you connected to the internet?')
+  print('Error: ' .. message)
 end
