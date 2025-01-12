6f256caca4097c385e5b11af6305fb92e75ca1ad
Tibor Claassen <tc@codebeige.net>
2025-01-12 14:56:55 +0100

    [asdf] Fix setup

--- zsh/config/plugins/asdf-direnv/asdf-direnv.plugin.zsh
+++ zsh/config/plugins/asdf-direnv/asdf-direnv.plugin.zsh
@@ -1,2 +1,15 @@
-export ASDF_DIR=$(brew --prefix asdf)/libexec
-source $(brew --prefix asdf)/etc/bash_completion.d/asdf.bash
+_asdf_prefix=$(brew --prefix asdf)
+ASDF_DIR="$_asdf_prefix/libexec"
+ASDF_COMPLETIONS="$_asdf_prefix/share/zsh/site-functions"
+unset _asdf_prefix
+
+# Load command
+if [[ -f "$ASDF_DIR/asdf.sh" ]]; then
+  source "$ASDF_DIR/asdf.sh"
+  # Load completions
+  if [[ -f "$ASDF_COMPLETIONS/_asdf" ]]; then
+    fpath+=("$ASDF_COMPLETIONS")
+    autoload -Uz _asdf
+    compdef _asdf asdf # compdef is already loaded before loading plugins
+  fi
+fi
