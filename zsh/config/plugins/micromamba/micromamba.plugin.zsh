17619f30892741fcbdb2e16d84db1bd35449703c
Tibor Claassen <tc@codebeige.net>
2025-06-30 13:34:59 +0200

    [zsh] Set up micromamba

---
+++ zsh/config/plugins/micromamba/micromamba.plugin.zsh
@@ -0,0 +1,10 @@
+export MAMBA_EXE="$(brew --prefix)/bin/micromamba"
+export MAMBA_ROOT_PREFIX="~/.local/share/mamba"
+
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__mamba_setup"
+else
+    alias micromamba="$MAMBA_EXE"  # Fallback on help from micromamba activate
+fi
+unset __mamba_setup
