7efa97c114733c7bfee8a958ff0b490642eba776
Tibor Claassen <tc@codebeige.net>
2025-09-17 09:34:10 +0200

    [nvim] Build rig.nvim in dev mode

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,47 +1,56 @@
 function fetch(repo, path)
   local url = 'https://github.com/' .. repo .. '.git'
-  if not vim.loop.fs_stat(path) then
-    print('Fetching ' .. url .. '...')
-    local result = vim.system(
-      {
-        'git',
-        'clone',
-        '--filter=blob:none',
-        '--branch=stable',
-        url,
-        path,
-      }, { text = true }
-    ):wait()
-    if result.code == 0 then
-      print('Successfully installed ' .. repo .. ' at ' .. path .. '.')
-    else
-      error('[' .. result.code .. '] ' .. result.stderr)
-    end
+  print('Fetching ' .. url .. '...')
+  local result = vim.system(
+    {
+      'git',
+      'clone',
+      '--filter=blob:none',
+      '--branch=stable',
+      url,
+      path,
+    }, { text = true }
+  ):wait()
+  if result.code == 0 then
+    print('Successfully installed ' .. repo .. ' at ' .. path .. '.')
+  else
+    error('[' .. result.code .. '] ' .. result.stderr)
   end
 end
 
-local function init ()
-  local rig_plugin_dir
-  local rig_plugin_dev_dir = vim.fs.normalize('~/src/rig.nvim')
-
-  if vim.loop.fs_stat(rig_plugin_dev_dir) then
-    rig_plugin_dir = rig_plugin_dev_dir
-  else
-    rig_plugin_dir = vim.fn.stdpath('data') .. '/lazy/rig.nvim'
-    if not vim.loop.fs_stat(rig_plugin_dir) then
-      fetch('codebeige/rig.nvim', rig_plugin_dir)
-      dofile(rig_plugin_dir .. "/build.lua")
-    end
+local function load_plugin(repo)
+  local name = string.match(repo, "/(.*)")
+  local dir = vim.fs.joinpath(vim.fn.stdpath('data'), "lazy", name)
+  if not vim.loop.fs_stat(dir) then
+    fetch(repo, dir)
   end
-  vim.opt.rtp:prepend(rig_plugin_dir)
+  vim.opt.rtp:prepend(dir)
+  return dir
+end
 
+local function load_rig()
+  local rig_dev_dir = vim.fs.normalize('~/src/rig.nvim')
+  local plugin_dir
+  if vim.loop.fs_stat(rig_dev_dir) then
+    plugin_dir = rig_dev_dir
+  else
+    plugin_dir = load_plugin('codebeige/rig.nvim')
+  end
+
+  local rig_lua = vim.fs.joinpath(plugin_dir, "lua/rig.lua")
+  if not vim.loop.fs_stat(rig_lua) then
+    dofile(vim.fs.joinpath(plugin_dir, "build.lua"))
+  end
+  vim.opt.rtp:prepend(plugin_dir)
+  return dir
+end
+
+local function init ()
+  load_rig()
   require('rig').setup()
   require('config.base').setup()
 
-  local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-  fetch('folke/lazy.nvim', lazypath)
-  vim.opt.rtp:prepend(lazypath)
-
+  load_plugin('folke/lazy.nvim')
   require('lazy').setup({
     spec = require('plugins'),
     dev = {
